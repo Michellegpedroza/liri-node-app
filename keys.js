9ed694a055b843cb1af657ec1dc03dfd11919758
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-10-22 13:58:33 -0700

    wrote JS for the Spotify api to retrieve the users song input.

--- keys.js
+++ keys.js
@@ -1,7 +1,10 @@
-
 console.log('this is loaded')
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.845ab30e87104387b9ca3e26384fc9d0,
+  secret: process.env.6bc5ea1dc11244fca8f8d11dd92a70d2
+}
+
+exports.omdb = {
+  api_key: process.env.http://www.omdbapi.com/?apikey=trilogy&
 }
--- liri.js
+++ liri.js
@@ -1,3 +1,34 @@
-require(`dotenv`).config()
-
+//Variables to require all node packages and files
+const dotenv = require(`dotenv`).config()
+const spotify = require(`node-spotify-api`)
+const moment = require(`moment`)
 const keys = require(`./keys.js`)
+const fs = require(`fs`)
+
+const spotify = new Spotify(key.spotify)
+const omdbKey = keys.omdb.API_KEY
+
+const firstCommand = process.argv[2]
+const secondCommand = process.argv[3]
+
+
+function spotifyThisSong(song) {
+  spotify.search({ type: 'track', query: song, limit: 1 }, function (e, data) {
+    if (e) {
+      console.log(`Error Occured:` + e)
+    }
+  } else {
+    for(let i = 0; i<data.tracks.items.length; i++) {
+    let songData = data.tracks.items[i]
+    //artist
+    console.log(`Artist: ` + songData.artists[0].name)
+    //song name
+    console.log(`Song: ` + songData.name)
+    //spotify preview link
+    console.log(`Preview URL: ` + songData.preview_url)
+    //album name
+    console.log(`Album: ` + songData.album.name)
+    console.log(`****************************`)
+  }
+});
+}
