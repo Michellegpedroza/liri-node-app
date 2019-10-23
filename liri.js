53c850def1f6682f66eead451860124324686ad7
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-10-23 12:51:05 -0700

    Wrote JS function to query for the bandsintown API and get the information the user is requesting

--- liri.js
+++ liri.js
@@ -1,35 +1,54 @@
-//Variables to require all node packages and files
+//Variables to require all node packages and import files
 const dotenv = require(`dotenv`).config()
-const fs = require(`fs`)
 const keys = require(`./keys.js`)
+const fs = require(`fs`)
+const axios = require(`axios`)
 const Spotify = require(`node-spotify-api`)
 const moment = require(`moment`)
 
 const spotify = new Spotify(keys.spotify)
 
 //Capture the users input
-var action = process.argv[2]
-var request = process.argv[3]
+let action = process.argv[2]
+let userRequest = process.argv[3]
 
 //Switch case for each instance
 switch (action) {
   case `concert-this`:
-    bandsInTown(request)
+    bandsInTown()
     break
 
   case `spotify-this-song`:
-    spotifyThisSong(request)
+    spotifyThisSong()
     break
 
   case `movie-this`:
-    movieThis(request)
+    movieThis()
     break
 
   case `do-what-it-says`:
-    doWhatItSays(request)
+    doWhatItSays()
     break
 
   case `default`:
     console.log(`These Instructions are not Valid`)
+    console.log(`Enter a valid search term such as: `)
+    console.log(`{concert-this}, {spotify-this-song}, {movie-this}, {do-what-it-says} followed by your request`)
     break
+}
+
+
+//Function to get the bandsintown API and return users request
+function bandsInTown() {
+  axios.get(`https://rest.bandsintown.com/artists` + userRequest + `/events?app_id=` + keys.bands.id)
+    .then(function (response) {
+      for (let i = 0; i < response.data.length; i++) {
+        console.log(`Venue Name: ` + response.data[i].venue.name)
+        console.log(`Venue Location: ` + response.data[i].venue.city + `, ` + response.data[i].venue.country)
+        console.log(`Date of the Event: ` + moment(response.data[i].datetime).format(`MM/DD/YYYY`))
+      }
+    })
+    .catch(e => {
+      console.log(e)
+    })
 }
