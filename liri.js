ea757aa1e19f0a56064ef4cb4444a4e58c5ce5d2
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-10-23 20:37:09 -0700

    added JS function to get the users song request and print the information

--- liri.js
+++ liri.js
@@ -8,47 +8,61 @@ const moment = require(`moment`)
 
 const spotify = new Spotify(keys.spotify)
 
-//Capture the users input
-let action = process.argv[2]
-let userRequest = process.argv[3]
+// Capture the users Input
+const action = process.argv[2]
+const userResponse = process.argv[3]
 
-//Switch case for each instance
+//Switch case for each action
 switch (action) {
   case `concert-this`:
-    bandsInTown()
+    concertThis()
     break
-
   case `spotify-this-song`:
-    spotifyThisSong()
+    spotifyThis()
     break
-
   case `movie-this`:
-    movieThis()
+    movies()
     break
-
   case `do-what-it-says`:
-    doWhatItSays()
+    doThis()
     break
-
-  case `default`:
-    console.log(`These Instructions are not Valid`)
-    console.log(`Enter a valid search term such as: `)
-    console.log(`{concert-this}, {spotify-this-song}, {movie-this}, {do-what-it-says} followed by your request`)
+  default:
+    console.log(`Enter a valid search term, such as: `)
+    console.log(`{concert-this},{spotify-this-song}, {movie-this}, or {do-what-it-says} followed by your request.`)
     break
 }
 
-
-//Function to get the bandsintown API and return users request
-function bandsInTown() {
-  axios.get(`https://rest.bandsintown.com/artists` + userRequest + `/events?app_id=` + keys.bands.id)
+//Function to get the bandsintown API & return users request
+function concertThis() {
+  axios.get(`https://rest.bandsintown.com/artists/` + userResponse + `/events?app_id=` + keys.bands.id)
     .then(function (response) {
       for (let i = 0; i < response.data.length; i++) {
+        //print the artists concert info
         console.log(`Venue Name: ` + response.data[i].venue.name)
         console.log(`Venue Location: ` + response.data[i].venue.city + `, ` + response.data[i].venue.country)
         console.log(`Date of the Event: ` + moment(response.data[i].datetime).format(`MM/DD/YYYY`))
       }
     })
-    .catch(e => {
-      console.log(e)
-    })
 }
+
+// Function to retrieve the users song request
+function spotifyThis() {
+  spotify.search({ type: `track`, query: userResponse }, function (err, data) {
+    if (err) {
+      console.log(err)
+    }
+
+    //Log users song request info
+    const songRequested = data.tracks.items
+    console.log(`Artist: ` + songRequested[0].artists[0].name)
+    console.log(`Song Name: ` + songRequested[0].name)
+    console.log(`Preview Link: ` + songRequested[0].preview_url)
+    console.log(`Album: ` + songRequested[0].album.name)
+  })
+
+  // If users request is empty 
+  if (userResponse === ``) {
+    userResponse = `The Sign`
+  }
+}
+
